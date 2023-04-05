423622a9d54bac4145f8cbf1b3ff8539ed0e6395
Cam Crow <19ccrow99@gmail.com>
2023-04-05 15:33:49 -0500

    feat: pulls all companies in January 2023

--- cron.js
+++ cron.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 export const warnCron = () => {
   cron.schedule('*/1 * * * *', () => {
-    axios.get('https://data.texas.gov/resource/8w53-c4f6.json')
+    axios.get('https://data.texas.gov/resource/8w53-c4f6.json?$where=notice_date between \'2023-01-01T12:00:00\' and \'2023-02-01T14:00:00\'')
       .then((res) => {
         console.log(res)
       })
