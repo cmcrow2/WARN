e5f9a0406706d63f64e722a8a07d1326a32349cf
Cam Crow <19ccrow99@gmail.com>
2023-04-06 15:27:07 -0500

    fix: cleanup, comments

--- server/warn-notify/cron.js
+++ server/warn-notify/cron.js
@@ -5,11 +5,12 @@ import moment from 'moment'
 
 // runs a cronjob everyday that pulls data from data.texas.gov
 export const warnCron = () => {
-  // use node-cron package to setup a job that runs (currently for testing) every minute
-  // use axios package to make a GET request to data.texas.gov for all companies that gave a WARN notice in 2023
+  // initialize date and query variables
   const startDate = '2023-01-01T00:00:00'
   const endDate = moment().format("YYYY-MM-DDT00:00:00")
   const query = getWarnByDateRange(startDate, endDate)
+  
+  // use axios package to make a GET request to data.texas.gov for all companies that gave a WARN notice in 2023
   axios.get(query, {
     headers: {
       'X-App-Token': process.env.APP_TOKEN
@@ -21,6 +22,8 @@ export const warnCron = () => {
   .catch((err) => {
     console.log(err)
   })
+
+  // currently, disable cron job until ready for schedule testing
   // cron.schedule('*/1 * * * *', () => {
   // })
 }
--- server/warn-notify/queries.js
+++ server/warn-notify/queries.js
@@ -1,3 +1,4 @@
+// gets WARN notices for Texas for a given time frame
 export const getWarnByDateRange = (startDate, endDate) => {
   return `${process.env.TEXAS_URL}$where=notice_date between '${startDate}' and '${endDate}'`
 }
