ded4215b472ce8a3f73153e7b10cc2dbd1a7c4c3
Cam Crow <19ccrow99@gmail.com>
2023-04-06 15:25:07 -0500

    feat: implemented moment

--- server/warn-notify/cron.js
+++ server/warn-notify/cron.js
@@ -1,23 +1,26 @@
-import cron from 'node-cron'
+// import cron from 'node-cron'
 import axios from 'axios'
-import {getWarnByDateRange} from './queries.js'
+import { getWarnByDateRange } from './queries.js'
+import moment from 'moment'
 
 // runs a cronjob everyday that pulls data from data.texas.gov
 export const warnCron = () => {
   // use node-cron package to setup a job that runs (currently for testing) every minute
-  cron.schedule('*/1 * * * *', () => {
-    // use axios package to make a GET request to data.texas.gov for all companies that gave a WARN notice during Jan 2023
-    const query = getWarnByDateRange('2023-01-01T12:00:00', '2023-02-01T14:00:00')
-    axios.get(query, {
-      headers: {
-        'X-App-Token': process.env.APP_TOKEN
-      }
-    })
-      .then((res) => {
-        console.log(res.data)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+  // use axios package to make a GET request to data.texas.gov for all companies that gave a WARN notice in 2023
+  const startDate = '2023-01-01T00:00:00'
+  const endDate = moment().format("YYYY-MM-DDT00:00:00")
+  const query = getWarnByDateRange(startDate, endDate)
+  axios.get(query, {
+    headers: {
+      'X-App-Token': process.env.APP_TOKEN
+    }
   })
+  .then((res) => {
+    console.log(res.data)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+  // cron.schedule('*/1 * * * *', () => {
+  // })
 }
